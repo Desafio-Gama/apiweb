3454d327db95df3517e1336d994482c46dd32eaf
daniel.silveira <daniel.repres@hotmail.com>
2022-05-09 14:46:18 -0300

    Ok

--- src/main/java/br/com/chocode/apiWeb/controller/UploadController.java
+++ src/main/java/br/com/chocode/apiWeb/controller/UploadController.java
@@ -8,18 +8,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-
 @Controller
 public class UploadController {
 
     @GetMapping("/home")
-    public String home(){
-        return "/home/index";
+    public String home() {
+        return "home/index";
     }
 
     @PostMapping("/upload")
     public String handleUploadForm(Model model, String description,
-                                   @RequestParam("file") MultipartFile multipart) {
+            @RequestParam("file") MultipartFile multipart) {
         String fileName = multipart.getOriginalFilename();
 
         System.out.println("Description: " + description);
